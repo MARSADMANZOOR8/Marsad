997bd7e2b32d8a6e87a91a85bdc96d46b36b811c
MARSADMANZOOR8 <120159246+MARSADMANZOOR8@users.noreply.github.com>
2022-12-18 06:15:43 -0800

    Add files via upload

---
+++ color.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <window.D>
+using namespace.std;
+int main()
+{
+system("color 46");
+cout<<" this is my text"<< endl;
+
+
+return 0;
+}
---
+++ creativity.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+main () 
+{
+cout << "    ============================================ " << endl ; 
+cout << "    ==        ==                              == " << endl ;
+cout << "    ==        ==         @@@@@                == " << endl ;
+cout << "    ==        ==        @                     == " << endl ;
+cout << "    ==        ==        @   *                 == " << endl ;
+cout << "    ==        ==        @                     == " << endl ; 
+cout << "    ==        ==         @@@@@                == " << endl ;
+cout << "    ==        ==                              == " << endl ; 
+cout << "    ==        ==                              == " << endl ;
+cout << "    ==        ==                              == " << endl ;
+cout << "    ==        ==                              == " << endl ;
+cout << "    ============================================ " << endl ;
+cout << "    ==                                         " << endl ;
+cout << "    ==                                         " << endl ; 
+cout << "    ==    PAKISTAN ZINDABAD                    " << endl ;
+cout << "    ==                                         " << endl ;
+cout << "    ==                                         " << endl ;
+cout << "    ==                                         " << endl ;
+cout << "    ==                                         " << endl ;
+cout << "    ==                                         " << endl ;
+cout << "    ==                                         " << endl ;
+cout << "    ==                                         " << endl ;
+cout << "    ==                                         " << endl ;
+cout << "    ==                                         " << endl ;
+cout << "    ==                                         " << endl ;
+}
+ 
+                         
